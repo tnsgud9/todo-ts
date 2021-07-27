e2a6428a846e4bb1ec86c3bc4680c4d2fc854fd6
tnsgud9 <tnsgud9@naver.com>
2021-07-27 19:31:35 +0900

    Update: data Structure

--- src/pages/Todo/index.tsx
+++ src/pages/Todo/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import TodoCreate from "./components/TodoCreate";
 const Todo = () => {
   return (
     <div>
       <h1>ToDo List</h1>
+      <TodoCreate />
     </div>
   );
 };
