37fe4a89e69e8a264595850e0029d6dd60b2e2b0
tnsgud9 <tnsgud9@naver.com>
2021-07-28 18:20:39 +0900

    Update: Todo Create

--- src/pages/Todo/index.tsx
+++ src/pages/Todo/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import TodoCreate from "./components/TodoCreate";
+import TodoList from "./components/TodoList";
 const Todo = () => {
   return (
     <div>
       <h1>ToDo List</h1>
       <TodoCreate />
+      <TodoList />
     </div>
   );
 };
